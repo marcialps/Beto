822aec1806dd5d20f88efe109d58af2fea5e2a7d
marcialps <marcialpsiqueira@gmail.com>
2026-09-14 16:10:54 -0300

    Separando configurações do BD

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCzqLgdNwc_0vMf-ewtRpfslQx9hZo0xEI",
+  authDomain: "agrovelbeto.firebaseapp.com",
+  projectId: "agrovelbeto",
+  storageBucket: "agrovelbeto.firebasestorage.app",
+  messagingSenderId: "322157187411",
+  appId: "1:322157187411:web:cb6b643c518ab8ba03d1a6",
+  measurementId: "G-W0FZ1DGPD3"
+};
